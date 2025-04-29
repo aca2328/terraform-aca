2186cfa853d5d8b6ea30c5989dfef355749fe930
Antoine Camerlo <aca2328@gmail.com>
2025-04-29 21:21:29 +0200

    add attach

--- alpaca.sh
+++ alpaca.sh
@@ -1,2 +1,3 @@
 docker container prune -f
-docker run --name alpaca -v "$PWD/:/home:rw" -v /var/run/docker.sock:/var/run/docker.sock -ti -d aca2328/alpaca:multi-arch
+docker run --name alpaca -v "$PWD/:/home:rw" -v /var/run/docker.sock:/var/run/docker.sock -d aca2328/alpaca:multi-arch
+docker attach alpaca
